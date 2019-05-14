1bd1d68bb5fcbd7048df8afa35c0c4609c54dc39
TolyaRuban <tolyaruban@gmail.com>
2019-05-14 17:08:38 +0300

    PhoneViewer added, Component extended, HideShow logic added

--- js/phones/phones-page.js
+++ js/phones/phones-page.js
@@ -1,4 +1,5 @@
 import PhonesCatalog from './components/phones-catalog.js';
+import PhoneViewer from './components/phone-viewer.js';
 import PhonesService from './services/phones-service.js';
 
 export default class PhonesPage {
@@ -7,8 +8,18 @@ export default class PhonesPage {
         this._render();
 
         this._catalog = new PhonesCatalog({
-            element: this._element.querySelector('[data-phone-catalog]'),
-            phones: PhonesService.getAll()
+            element: this._element.querySelector('[data-component="phone-catalog"]'),
+            phones: PhonesService.getAll(),
+            onPhoneSelected: (id) => {
+                console.log('Selected: ', id);
+                const phoneDetails = PhonesService.getById(id);
+                this._catalog.hide();
+                this._viewer.show(phoneDetails);
+            }
+        })
+
+        this._viewer = new PhoneViewer({
+            element: this._element.querySelector('[data-component="phone-viewer"]')
         })
     }
 
@@ -16,37 +27,38 @@ export default class PhonesPage {
         this._element.innerHTML = `
         <div class="row">
 
-        <!--Sidebar-->
-        <div class="col-md-2">
-            <section>
-            <p>
-                Search:
-                <input>
-            </p>
+            <!--Sidebar-->
+            <div class="col-md-2">
+                <section>
+                <p>
+                    Search:
+                    <input>
+                </p>
 
-            <p>
-                Sort by:
-                <select>
-                <option value="name">Alphabetical</option>
-                <option value="age">Newest</option>
-                </select>
-            </p>
-            </section>
+                <p>
+                    Sort by:
+                    <select>
+                    <option value="name">Alphabetical</option>
+                    <option value="age">Newest</option>
+                    </select>
+                </p>
+                </section>
 
-            <section>
-            <p>Shopping Cart</p>
-            <ul>
-                <li>Phone 1</li>
-                <li>Phone 2</li>
-                <li>Phone 3</li>
-            </ul>
-            </section>
-        </div>
+                <section>
+                <p>Shopping Cart</p>
+                <ul>
+                    <li>Phone 1</li>
+                    <li>Phone 2</li>
+                    <li>Phone 3</li>
+                </ul>
+                </section>
+            </div>
 
-        <!--Main content-->
-        <div class="col-md-10">
-            <div data-phone-catalog></div>
-        </div>
+            <!--Main content-->
+            <div class="col-md-10">
+                <div data-component="phone-viewer"></div>
+                <div data-component="phone-catalog"></div>        
+            </div>
         </div>`
     }
 }
