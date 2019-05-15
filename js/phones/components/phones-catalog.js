d2d991205d9a69530f85071f7c21f26e93459528
TolyaRuban <tolyaruban@gmail.com>
2019-05-15 12:34:34 +0300

    subscribe/emit almost implemented

--- js/phones/components/phones-catalog.js
+++ js/phones/components/phones-catalog.js
@@ -4,20 +4,19 @@ export default class PhonesCatalog extends Component {
     constructor({
         element,
         phones = [],
-        onPhoneSelected = () => { }
     }) {
         super({ element });
         this._phones = phones;
-        this.onPhoneSelected = onPhoneSelected;
+
         this._render();
 
         this.on("click", '[data-element="details-link"]', event => {
           const phoneEl = event.target.closest('[data-element="phone-element"]');
           const phoneId = phoneEl.dataset.phoneId;
-          this.onPhoneSelected(phoneId);
+        this.emit("phone-selected", phoneId);
         });
     }
-
+    
     _render() {
         this._element.innerHTML = `
         <ul class="phones">
